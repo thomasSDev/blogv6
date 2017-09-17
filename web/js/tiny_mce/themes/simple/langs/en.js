c3c6945163d8af0214d5a1cfe7a9466b37635abd
thomasSDev <sutre.thomas.29@gmail.com>
2017-09-17 20:11:08 +0200

    Ajout formulaire Tiny_MCE, reste à mieuc le paramétrer et à regrouper les formulaires du blog pour n'en avoir qu'un

---
+++ web/js/tiny_mce/themes/simple/langs/en.js
@@ -0,0 +1,11 @@
+tinyMCE.addI18n('en.simple',{
+bold_desc:"Gras (Ctrl+B)", 
+italic_desc:"Italique (Ctrl+I)", 
+underline_desc:"Souligne (Ctrl+U)", 
+striketrough_desc:"Barre", 
+bullist_desc:"Liste non numerotee", 
+numlist_desc:"Liste numerotee", 
+undo_desc:"Annuler la derniere action (Ctrl+Z)", 
+redo_desc:"Refaire la derniere action (Ctrl+Y)", 
+cleanup_desc:"Debugguer le code" 
+});
