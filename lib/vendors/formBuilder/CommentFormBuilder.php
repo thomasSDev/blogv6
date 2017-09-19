16bd2a5acd3fd738de74e0e3f782eff400f07c57
thomasSDev <sutre.thomas.29@gmail.com>
2017-09-19 23:58:45 +0200

    formulaires en un objet, fo,nctionnel mais tiny_mce ne prend pas

---
+++ lib/vendors/formBuilder/CommentFormBuilder.php
@@ -0,0 +1,33 @@
+<?php
+namespace formBuilder;
+ 
+use \fram\FormBuilder;
+use \fram\StringField;
+use \fram\TextField;
+use \fram\MaxLengthValidator;
+use \fram\NotNullValidator;
+ 
+class CommentFormBuilder extends FormBuilder
+{
+  public function build()
+  {
+    $this->form->add(new StringField([
+        'label' => 'Auteur',
+        'name' => 'auteur',
+        'maxLength' => 50,
+        'validators' => [
+          new MaxLengthValidator('L\'auteur spécifié est trop long (50 caractères maximum)', 50),
+          new NotNullValidator('Merci de spécifier l\'auteur du commentaire'),
+        ],
+       ]))
+       ->add(new TextField([
+        'label' => 'Contenu',
+        'name' => 'contenu',
+        'rows' => 7,
+        'cols' => 50,
+        'validators' => [
+          new NotNullValidator('Merci de spécifier votre commentaire'),
+        ],
+       ]));
+  }
+}
